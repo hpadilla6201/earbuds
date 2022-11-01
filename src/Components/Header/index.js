2ba22154466b5d06b64be05affd5173f0a918422
Hector Padilla <hpadilla6201@gmail.com>
2022-11-01 14:01:20 -0400

    finished header

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,6 +1,32 @@
 const Header = () => {
     return (
-        <h1>headers</h1>
+        <header> 
+            <nav class="navbar navbar-expand-lg bg-blue">
+            <div class="col-3">
+                <h1>earbuds logo</h1>
+            </div>
+            <div className="col-6">
+                <div class="col-10">
+                <form class="form-inline">
+                    <input class="form-control" type="search" placeholder="Search" aria-label="Search"/>
+                </form>
+                </div>
+                
+            </div>
+            <div className="col-3">
+                <div class="btn-group">
+                    <button type="button" class="btn btn-primary dropdown-toggle"  data-bs-toggle="dropdown">
+                        Profile
+                    </button>
+                    <div class="dropdown-menu">
+                        <a class="dropdown-item" href="./index.js">Action</a>
+                        <a class="dropdown-item" href="./index.js">Another action</a>
+                        <a class="dropdown-item" href="./index.js">Something else here</a>
+                    </div>
+                </div>
+            </div>
+          </nav>
+        </header>
     );
 };
 
